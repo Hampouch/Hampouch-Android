a24efccdad57d5d707c30c4f55c46fb824c400d0
about-kim <kimkorea2nd@gmail.com>
2026-07-27 17:06:57 +0900

    fix: dev pull 및 홈화면 navbar innerpadding 수정

--- app/src/main/java/com/example/hampouch/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/hampouch/ui/home/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -70,7 +71,8 @@ fun HomeScreen(
                 BottomNavBar(
                     selectedItem = selectedBottomTab,
                     onItemSelected = { selectedBottomTab = it },
-                    onAddClick = {}
+                    onAddClick = {},
+                    modifier = Modifier.navigationBarsPadding()
                 )
             }
         }
